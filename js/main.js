edf832289d4791db369e7d3893362b5f95ac9c41
Zahar Sysyuk <vip.sysyuk@mail.ru>
2021-11-21 14:40:42 +0700

    реализовал загрузку файлов .xlsx во вкладке - Загрузить

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-import TableCsv from "./TableCsv.js";
+import TableBuilder from "./TableCsv.js";
 
 const tableRoot = document.querySelector("#tableRoot");
-const csvFileInput = document.querySelector("#fileInput");
-const tableCsv = new TableCsv(tableRoot);
+const fileInput = document.querySelector("#fileInput"); // only .csv, .xlsx, .txt
+const tableBuilder = new TableBuilder(tableRoot);
 
-csvFileInput.addEventListener("change", (e) => {
-    Papa.parse(csvFileInput.files[0], {
-        delimiter: ";",
-        skipEmptyLines: true,
-        complete: (results) => {
-            tableCsv.update(results.data.slice(1), results.data[0]);
-            console.log(results);
-          }
-    });
+fileInput.addEventListener("change", (e) => {
+    console.log(fileInput.files[0].name);
+
+    if (fileInput.files[0].name.includes(".csv")) {
+        Papa.parse(fileInput.files[0], {
+            delimiter: ";",
+            skipEmptyLines: true,
+            complete: (results) => {
+                tableBuilder.update(results.data.slice(1), results.data[0]);
+                console.log(results);
+              }
+        });
+    }
+    else if (fileInput.files[0].name.includes(".xlsx")) {
+        readXlsxFile(fileInput.files[0]).then(function(data) {
+            tableBuilder.update(data.slice(1), data[0]);
+            console.log(data);
+        });
+    }
 });
