e127c94f9229273589b935211d43c1da16559411
Zahar Sysyuk <vip.sysyuk@mail.ru>
2021-11-23 13:17:59 +0700

    реализовал загрузку файлов .txt во вкладке - Загрузить

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const tableRoot = document.querySelector("#tableRoot");
 const fileInput = document.querySelector("#fileInput"); // only .csv, .xlsx, .txt
 const tableBuilder = new TableBuilder(tableRoot);
 
-fileInput.addEventListener("change", (e) => {
-    console.log(fileInput.files[0].name);
+fileInput.addEventListener("change", (e) => {  
+    let fileName = fileInput.files[0].name;
+    console.log(fileName);
 
-    if (fileInput.files[0].name.includes(".csv")) {
+    if (fileName.includes(".csv")) {
         Papa.parse(fileInput.files[0], {
             delimiter: ";",
             skipEmptyLines: true,
@@ -17,10 +18,31 @@ fileInput.addEventListener("change", (e) => {
               }
         });
     }
-    else if (fileInput.files[0].name.includes(".xlsx")) {
-        readXlsxFile(fileInput.files[0]).then(function(data) {
+    else if (fileName.includes(".xlsx")) {
+        readXlsxFile(fileInput.files[0]).then((data) => {
             tableBuilder.update(data.slice(1), data[0]);
             console.log(data);
         });
+    }
+    else if (fileName.includes(".txt")) {
+        const file = fileInput.files[0];
+
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            const file = e.target.result;
+            const lines = file.split(/\r\n|\n/);
+            console.log(lines);
+
+            let data = [];
+            for (let i = 0; i < lines.length; i++) 
+                data[i] = lines[i].split(" ");
+            
+            tableBuilder.update(data.slice(1), data[0]);
+        };        
+        reader.onerror = (e) => alert(e.target.error.name);
+        reader.readAsText(file); 
+    }
+    else {
+        alert("Такой формат не доступен!");
     }
 });
